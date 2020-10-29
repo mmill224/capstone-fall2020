f34bf4fd84a1255a3205162d487ee09545e2dc88
adipio <adipio@kent.edu>
2020-10-28 17:24:33 -0700

    Password check
    I wrote a function to test the strength of a password. It returns true if the password is strong and false if the password is weak.

---
+++ database/Passwordcheck.py
@@ -0,0 +1,35 @@
+import re
+
+def passwordChecker(password):
+    #check the password length
+    if len(password) < 8:
+        return False
+
+    #checks for lower
+    lowerCheck = re.compile(r'[a-z]')
+    if not(lowerCheck.search(password)):
+        return False
+
+    #checks for upper
+    upperCheck = re.compile(r'[A-Z]')
+    if not(upperCheck.search(password)):
+        return False
+
+    #checks for number
+    numCheck = re.compile(r'\d')
+    if not(numCheck.search(password)):
+        return False
+
+    #checks for special character
+    spCheck = re.compile(r'\W')
+    if not(spCheck.search(password)):
+        return False
+
+    else:
+        return True
+if __name__ == '__main__':
+    password = input("Enter your password.")
+    if passwordChecker(password) is True:
+        print("That is a strong password. ")
+    else:
+        print("That is a weak password.")
