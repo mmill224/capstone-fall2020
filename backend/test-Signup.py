d691618b4704d8cf40bdde48682c6194a64bf4bf
mmill224 <mmill224@kent.edu>
2020-11-15 18:34:34 -0500

    Update test-Signup.py
    Adds a test that should be pretty reusable

--- backend/test-Signup.py
+++ backend/test-Signup.py
@@ -1,4 +1,4 @@
-import Signup
+import Signup as thingToTest
 
 example_data1 = {
     "username":     "mattMiller",
@@ -10,10 +10,28 @@ example_data1 = {
     "password2":    "Dr.SambaIsAmazing123",
 }
 
+
+# use this to remove any records that we don't want permanently.
+# this doesn't really work yet
+def cleanUp(username):
+    my_cursor = fpdatabase.cursor()
+    sql = "DELETE FROM user WHERE userID = %s"
+    my_cursor.execute(sql, username)
+    fpdatabase.commit()
+
+
+
 def testSignup(username, firstname, middlename, lastname, email, password, password2):
-    print("Testing signUp()... /n")
-    signUp(username, firstname, middlename, lastname, email, password, password2)
+    print("Testing signUp()... \n")
+    thingToTest.signUp(username, firstname, middlename, lastname, email, password, password2)
     return
 
-testSignup(example_data1.username, example_data1.firstname, example_data1.middlename, example_data1.lastname, example_data1.email, example_data1.password, example_data1.password2)
+
+test = input("Would you like to test signUp? [y/n]: ")
+
+if(test == 'y'):
+    try:
+        testSignup(example_data1["username"], example_data1["firstname"], example_data1["middlename"], example_data1["lastname"], example_data1["email"], example_data1["password"], example_data1["password2"])
+    except:
+        pass
 
